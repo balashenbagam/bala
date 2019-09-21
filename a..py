1c7903ac0572ef64b477f139fea97d677fd3a836
balashenbagam <54837426+balashenbagam@users.noreply.github.com>
2019-09-21 14:11:33 +0530

    Add files via upload

--- a..py
+++ a..py
@@ -1,3 +1,14 @@
-binary = input("Enter number in Binary Format: ")
-temp = int(binary, 2)
-print(oct(temp))
+n = int(input())
+L = [int(x) for x in input().split()]
+q = int(input())
+for i in range(q) :
+    L2 = [int(x) for x in input().split()]
+    if L2[0]==1 :
+        _,a,b,x = L2
+        for j in range(a,b+1) :
+            L[j] += x
+    elif L2[0]==2 :
+        _, a, b = L2
+        max1 = max(L[a:b+1])
+print(max1,end='')
+
